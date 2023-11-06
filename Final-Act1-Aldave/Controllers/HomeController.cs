2aa56538a6a6ebdc775d69e5c9bf91bca86fa582
fwedwicc <moreno.frederick.capiral@gmail.com>
2023-11-06 19:43:40 +0800

    Add project files.

---
+++ Final-Act1-Aldave/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final_Act1_Aldave.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Inquiry()
+        {
+            return View();
+        }
+        public ActionResult Purchase()
+        {
+            return View();
+        }
+    }
+}
---
+++ Final-Act1-Aldave/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Final_Act1_Aldave.MvcApplication" Language="C#" %>
